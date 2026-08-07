f48a4d8b27cb709b9f6b556d4888e8978f6b13d6
Michael <michaelflexzy11@gmail.com>
2026-08-07 04:20:50 +0100

    added proper error handling plus  a filter route ro filter by title

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     try {
-        res.render('index');
+        res.render('index', { err: null });
     } catch (err) {
         next(err)
     }
@@ -22,21 +22,38 @@ router.get('/projects', async (req, res, next) => {
 
         return data.length === 0
             ? res.redirect('/projects/new')
-            : res.render('projects/project', { dataBase: data });
+            : res.render('projects/project', { dataBase: data, err: null });
     } catch (err) {
+        res.render('projects/project', { dataBase: null, err: err.message });
+        next(err)
+    }
+});
+
+router.get('/projects/filter', async (req, res, next) => {
+    try {
+        const { title } = req.query;
+        const result = await db.query("SELECT * FROM public.projects WHERE title ILIKE '%' || $1 || '%'", [title.trim()]);
+        const data = result.rows;
+
+        return data.length === 0
+            ? res.render('projects/project', { dataBase: data, err: 'No cards matched this title'.toUpperCase() })
+            : res.render('projects/project', { dataBase: data, err: null });
+    } catch (err) {
+        res.render('projects/project', { dataBase: null, err: err.message });
         next(err)
     }
 });
 
 router.get('/projects/new', (req, res, next) => {
     try {
-        res.render('projects/new');
+        res.render('projects/new', { err: null });
     } catch (err) {
+        res.render('projects/new', { err: err.message });
         next(err);
     }
 });
 
-router.post('/projects', async (req, res) => {
+router.post('/projects', async (req, res, next) => {
     try {
         const { title, stack } = req.body;
         const status = ["Stable", "In Progress", "Breaking"];
@@ -45,38 +62,54 @@ router.post('/projects', async (req, res) => {
 
         let healthScore = Math.floor(Math.random() * 100) + 1;
 
-        const result = await db.query(
+        const input = await db.query(
             'INSERT INTO public.projects (id, title, tech_stack, status, health_score) VALUES ($1, $2, $3, $4, $5)',
-            [uuidv4(), title, stack, randomStatus, healthScore]
+            [uuidv7(), title, stack, randomStatus, healthScore]
         );
-        res.redirect('/projects');
 
+        const result = await db.query(
+            'SELECT * FROM public.projects',
+        );
+        const data = result.rows;
+
+        return data.length === 0
+            ? res.redirect('/projects/new')
+            : res.render('projects/project', { dataBase: data, err: null });
     } catch (err) {
-        console.error('Database query error:', err.message);
-        res.status(500).json({ error: 'Internal server error' });
+        res.render('projects/project', { dataBase: null, err: err.message });
+        next(err);
     }
 });
 
-router.get('/projects/:id', async (req, res) => {
+router.get('/projects/:id', async (req, res, next) => {
     try {
         const { id } = req.params;
         const result = await db.query('SELECT * FROM public.projects WHERE id = $1', [id]);
         const data = result.rows;
 
-        res.render('projects/show', { data: data[0] });
+        res.render('projects/show', { data: data[0], err: null });
     } catch (err) {
-        res.status(500).json({ error: err.message });
+        res.render('projects/show', { data: null, err: err.message });
+        next(err);
     }
 });
 
-router.delete('/projects/:id', async (req, res) => {
+router.delete('/projects/:id', async (req, res, next) => {
     try {
         const { id } = req.params;
-        const result = await db.query('DELETE FROM public.projects WHERE id = $1', [id]);
+        await db.query('DELETE FROM public.projects WHERE id = $1', [id]);
 
-        res.redirect('/projects');
+        const result = await db.query(
+            'SELECT * FROM public.projects',
+        );
+        const data = result.rows;
+
+        return data.length === 0
+            ? res.redirect('/projects/new')
+            : res.render('projects/project', { dataBase: data, err: null });
     } catch (err) {
-        res.status(500).json({ error: err.message });
+        res.render('projects/project', { dataBase: null, err: err.message });
+        next(err);
     }
 });
 
